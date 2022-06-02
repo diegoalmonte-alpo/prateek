4275e09a23e54a40efaaed39de3a60f94ee2c030
diegoalmonte-alpo <62121801+diegoalmonte-alpo@users.noreply.github.com>
2022-06-02 19:02:58 -0400

    Add files via upload

---
+++ ap_account_bank_statement_import_ofx/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Account Bank Statement Import',
+    'category': 'Account',
+    'description':"""
+Import OFX Bank Statement.  Version 13 
+""",
+    'author': 'Alpo Multimedia, SRL',
+    'website': 'https://alpo.com.do/',
+    'version': '1.0.0',
+    'depends': ['account_bank_statement_import'],
+    'data' : [],
+    'qweb': [],
+    'auto_install': False,
+    'installable': True,
+    'application': True,
+
+}
